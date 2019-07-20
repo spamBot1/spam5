2a237b4e347b427be00c157ecfba8a6e96d4d55c
spamBot1 <45138785+spamBot1@users.noreply.github.com>
2019-07-20 15:22:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -34,6 +34,24 @@ client.on('message', message => {
     }
 });
 
+});
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+  
+if (command == "say") {
+if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('?|**\`ADMINISTRATOR\`ليس لديك صلاحيات`**');
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
+
 client2.on('message', message => {
     if(message.content === '-راتب'){
         message.channel.send('#daily')
